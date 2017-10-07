6a08fbccdf2916133bf834c982753e3b76cc2342
sam-you <saddam.jbawi@gmail.com>
2017-10-07 13:39:24 +0300

    therteennth attempt with heroku

--- database/index.js
+++ database/index.js
@@ -1,6 +1,7 @@
 //This file for connecting to the database
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://admin:admin@ds113435.mlab.com:13435/books');
+var mongoUrl=process.env.MONGODB_URI||'mongodb://admin:admin@ds113435.mlab.com:13435/books'
+mongoose.connect(mongoUrl);
 //mongoose.connect('mongodb://localhost/Books');
 
 var db = mongoose.connection;
