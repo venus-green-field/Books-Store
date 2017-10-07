8af36e42edd10a3e35370d28f834e54779bece09
sam-you <saddam.jbawi@gmail.com>
2017-10-07 13:51:49 +0300

    sixteenenth attempt with heroku

--- database/index.js
+++ database/index.js
@@ -1,8 +1,10 @@
 //This file for connecting to the database
 var mongoose = require('mongoose');
-var mongoUrl=process.env.MONGODB_URI||'mongodb://admin:admin@ds113435.mlab.com:13435/books'
+var mongoUrl=process.env.MONGODB_URI||'mongodb://localhost/Books'
 mongoose.connect(mongoUrl);
 //mongoose.connect('mongodb://localhost/Books');
+//
+//mongodb://<dbuser>:<dbpassword>@ds113435.mlab.com:13435/books
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
